0ec92d6b636a22463732dd0fd66bfaffa0838a59
Prerna Jaiswal <prerna11081@iiitd.ac.in>
2014-06-08 20:53:07 +0530

    edit gemfile, routs.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
-App1::Application.routes.draw do
-  resources :posts
+App1::Application.routes.draw do |map|
+  map.resources :posts
+  map.resources :home
+  map.root :controller => "home";
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+
+
+   #root 'welcome#index'
+   #get 'static_pages/'
+   match '/', to: 'static_pages#home', via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
